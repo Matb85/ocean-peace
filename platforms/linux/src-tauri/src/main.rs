b4022b9a946fe421bf264d6b8964c55c7841b59b
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2022-05-01 22:31:05 +0200

    feat: :construction: event hadler for view change

--- platforms/linux/src-tauri/src/main.rs
+++ platforms/linux/src-tauri/src/main.rs
@@ -3,11 +3,31 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+// the payload type must implement `Serialize` and `Clone`.
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
+}
+
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      // listen to the `event-name` (emitted on any window)
+      let id = app.listen_global("OnChangeView", |event| {
+      //   let viewName: String = ["{:?}", &event.payload()];
+      //   app.emit_all("ChangeView", Payload { message: viewName.into()}).unwrap();
+      });
+      // unlisten to the event using the `id` returned on the `listen_global` function
+      // an `once_global` API is also exposed on the `App` struct
+      app.unlisten(id);
+      Ok(())
+    })
+
     .invoke_handler(tauri::generate_handler![test_command])
+
     .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .expect("failed to run app");
 }
 
 use serde::ser::{Serialize};
@@ -18,3 +38,6 @@ fn test_command(mut i: i8) -> i8{
   i=i+1;
   i.into()
 }
+
+
+
--- platforms/linux/src/App.svelte
+++ platforms/linux/src/App.svelte
@@ -1,126 +1,21 @@
 <script lang="ts">
-  import logo from './assets/logo.svg'
-  import photoNull from './assets/null_photo.svg'
-  import background  from './assets/background.svg'
+  import { emit, listen } from '@tauri-apps/api/event'
 
+  import MainPage from './MainPage.svelte';
 
-  import { invoke } from '@tauri-apps/api/tauri'
+  let currentActiveView: string = 'MainPage';
 
-  let lol = 0;
-  function OnClick(): void {
-    console.dir(window)
-    invoke('test_command',{i: lol}).then ((message) => lol=(message));
-  }
+  const unlisten = listen('ChangeView', event => {
+    currentActiveView = event.payload as string;
+  })
 </script>
 
 <main>
-  <!-- Background -->
-  <div class="bg">
-    <img src={background} alt="Svelte Logo" />
-  </div>
-
-  
-  <!-- Navigation Bar -->
-  <div class="nav">
-
-    <div class="profile">
-      <img class="pic" src={photoNull} alt="Svelte Logo" />
-      <p class="name">John Doe</p>
-    </div>
-
-    <div class="logo">
-      <img class="pic" src={logo} alt="Svelte Logo" />
-      <p class="text">OceanPeace</p>
-    </div>
-    <button style="pointer-events: initial;" on:click={OnClick}>{lol}</button>
-  </div>
-  <p>{lol}</p>
+  {#if currentActiveView == 'MainPage'}
+    <MainPage/> 
+  {/if}
 </main>
 
 <style>
-  *{
-    user-select: none !important;
-    -webkit-user-select: none !important;
-    pointer-events: none;
-  }
-
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-  /* Background */
-  .bg {
-    height: 100vh;
-    width: 100vw;
-    position: absolute;
-    left: 0%;
-    top: 0%;
-    overflow: hidden;
-  }
-  .bg img {
-    height: max-content;
-    width: max-content;
-  }
-
-  /* Navigation Bar */
-  .nav {
-    height: 100vh;
-    width: 308px;
-    background-color: #FFFFFF;
-    border-radius: 0px 32px 32px 0px;
-    position: absolute;
-    top: 0%;
-    left: 0%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .nav .profile {
-    width: 171px;
-    position: absolute;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    top: 98px;
-  }
-  .nav .profile .pic {
-    position: relative;
-    height: 171px;
-    width: 171px;
-    filter: drop-shadow(0px 2px 18px rgba(0, 0, 0, 0.25));
-    border-radius: 24px;
-  }
-  .nav .profile .name {
-    width: 171px;
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 21px;
-    line-height: 120%;
-    text-align: center;
-  }
-
-  .nav .logo {
-    height: 35px;
-    width: 210px;
-    top: 35px;
-    position: absolute;
-    display: flex;
-    flex-direction: row;
-  }
-  .nav .logo .pic {
-    height: 35px;
-    width: 42px;
-  }
-  .nav .logo .text {
-    height: 35px;
-    width: 161px;
-    font-family: 'Lato';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 25px;
-    line-height: 35px;
-    margin: 0px 7px;
-  }
+  
 </style>
