f1f5badf337384c33681a7099e9d2df345c6a99d
matb85 <mateuszbis85@gmail.com>
2022-05-04 22:53:39 +0200

    feat: :sparkles: add routify

--- platforms/linux/vite.config.ts
+++ platforms/linux/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import routify from "@roxi/routify/vite-plugin";
+
+const production = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    routify({ devHelper: !production }),
+    svelte({
+      emitCss: false,
+      compilerOptions: { dev: !production },
+    }),
+  ],
   build: {
     outDir: "build/",
   },
