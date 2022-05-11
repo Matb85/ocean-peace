fc223b3f59fc0fc32f963860c2a0304bac41ec42
Raccoon <96393651+Racuun@users.noreply.github.com>
2022-05-11 16:53:57 +0200

    Main page (#21)
    * feat: :tada: First front-back interaction
    
    * feat: :construction: event hadler for view change
    
    * feat: :sparkles: basics of Mayo alghoritm
    
    * refactor: :recycle: Delete MainPage for some reasons
    
    * feat: :sparkles: mayo update
    
    Mayo algorithm update - reading active window exe localization
    
    * fix: :wrench: fix Cargo.lock
    
    Co-authored-by: matb85 <mateuszbis85@gmail.com>

--- platforms/linux/src-tauri/src/main.rs
+++ platforms/linux/src-tauri/src/main.rs
@@ -3,8 +3,48 @@
   windows_subsystem = "windows"
 )]
 
-fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+mod mayo;
+use sysinfo::{Pid, PidExt, ProcessExt, System, SystemExt};
+use active_win_pos_rs::{get_active_window};
+
+use tauri::Manager;
+// the payload type must implement `Serialize` and `Clone`.
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
 }
+
+fn main() {
+
+  mayo::run_process();
+
+  tauri::Builder::default()
+    .setup(|app| {
+      // listen to the `event-name` (emitted on any window)
+      let id = app.listen_global("OnChangeView", |event| {
+      //   let viewName: String = ["{:?}", &event.payload()];
+      //   app.emit_all("ChangeView", Payload { message: viewName.into()}).unwrap();
+      });
+      // unlisten to the event using the `id` returned on the `listen_global` function
+      // an `once_global` API is also exposed on the `App` struct
+      app.unlisten(id);
+      Ok(())
+    })
+
+    .invoke_handler(tauri::generate_handler![test_command])
+
+    .run(tauri::generate_context!())
+    .expect("failed to run app");
+}
+
+use serde::ser::{Serialize};
+use serde::Deserialize;
+
+#[tauri::command]
+fn test_command(mut i: i8) -> i8{
+  i=i+1;
+  i.into()
+}
+
+
+
