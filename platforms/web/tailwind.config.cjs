d652fd882bb81fa2037f9922cb3bcd5add452d18
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2022-05-19 18:05:20 +0200

    Squashed commit of the following:
    commit f96b5ddae27e8bb0fa10adc0ef3a0cf33b1a0c22
    Author: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Date:   Thu May 19 12:37:19 2022 +0200
    
        chore(deps): update tauri monorepo (#29)
    
        Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    commit 3cdd4d86c9ad30499c70d4b3dc0453af3f570d37
    Author: Raccoon <96393651+Racuun@users.noreply.github.com>
    Date:   Thu May 19 12:35:40 2022 +0200
    
        fix: :art: Mayo warnings fix (#31)
    
    commit 06dc6442165c6be5dde4d04c4844465e993a3ae6
    Author: matb85 <mateuszbis85@gmail.com>
    Date:   Thu May 19 12:02:52 2022 +0200
    
        fix: :bug: fix tailwindcss setup

--- platforms/web/tailwind.config.cjs
+++ platforms/web/tailwind.config.cjs
@@ -1,1 +1,3 @@
-module.exports = require("@redinnlabs/system/utils/tailwind.config.cjs");
+const config = require("@redinnlabs/system/utils/tailwind.config.cjs");
+config.content = ["./src/**/*", "./node_modules/@redinnlabs/system/**/*"];
+module.exports = config;
