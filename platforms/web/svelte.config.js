42339b8b00919d7f6edb52fedabdbaf6bb639bb3
matb85 <mateuszbis85@gmail.com>
2022-06-08 15:55:26 +0200

    refactor(web): :building_construction: migrate to sveltekit

--- platforms/web/svelte.config.js
+++ platforms/web/svelte.config.js
@@ -1,7 +1,13 @@
 import sveltePreprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess({ postcss: true }),
+  kit: {
+    floc: true,
+    adapter: adapter({ fallback: "index.html" }),
+    prerender: {},
+  },
 };
