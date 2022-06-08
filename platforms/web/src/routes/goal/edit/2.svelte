0d5f2264992ccf1911c12ac86d817e39c3463df4
matb85 <mateuszbis85@gmail.com>
2022-06-08 16:53:11 +0200

    refactor(web): :recycle: refactor goals-related components

--- platforms/web/src/routes/goal/edit/2.svelte
+++ platforms/web/src/routes/goal/edit/2.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
   import { BackButton, Button, Heading, Aquarium } from "@redinnlabs/system/Elements";
-  import { RadioInput, TimeInput, CheckMultiple } from "@redinnlabs/system/Form";
+  import { RadioInput, TimeInput, CheckMultiple, timeInputConfig } from "@redinnlabs/system/Form";
+
+  const hours = timeInputConfig.hoursConfig();
+  const minutes = timeInputConfig.minutesConfig();
+  let h;
+  let m;
+  h = hours.current;
+  m = minutes.current;
+  Object.defineProperty(hours, "current", { set: val => (h = val), get: () => h });
+  Object.defineProperty(minutes, "current", { set: val => (m = val), get: () => m });
 
   let type;
   let limit;
@@ -11,105 +20,30 @@
   <Aquarium percent={80} />
 </div>
 
-<div class="content">
-  <div class="title">
-    <Heading tag={3}>Goal - Edit</Heading>
-  </div>
+<Heading tag={3} className="mt-7">Goal - Edit</Heading>
 
-  <div class="days">
-    <Heading tag={5} className="text-center !font-normal">Select Days</Heading>
-    <CheckMultiple
-      className="flex-wrap justify-center"
-      options={[
-        { label: "Mon", isChecked: true },
-        { label: "Tue", isChecked: false },
-        { label: "Wed", isChecked: false },
-        { label: "Thu", isChecked: false },
-        { label: "Fri", isChecked: false },
-        { label: "Sat", isChecked: false },
-        { label: "Sun", isChecked: false },
-      ]}
-    />
-  </div>
+<Heading tag={5} className="text-center !font-normal">Select Days</Heading>
 
-  <div class="limit">
-    <Heading tag={5} className="text-center !font-normal">Select Limit</Heading>
-    <RadioInput bind:chosen={limit} options={[{ value: "Time period" }, { value: "Times opened" }]} />
-    <TimeInput />
-  </div>
+<CheckMultiple
+  className="flex-wrap justify-center"
+  options={[
+    { label: "Mon", isChecked: true },
+    { label: "Tue", isChecked: false },
+    { label: "Wed", isChecked: false },
+    { label: "Thu", isChecked: false },
+    { label: "Fri", isChecked: false },
+    { label: "Sat", isChecked: false },
+    { label: "Sun", isChecked: false },
+  ]}
+/>
 
-  <div class="type">
-    <Heading tag={5} className="text-center !font-normal">Limit Type</Heading>
-    <RadioInput bind:chosen={type} options={[{ value: "Soft" }, { value: "Hard" }]} />
-  </div>
+<Heading tag={5} className="text-center !font-normal">Select Limit</Heading>
+<RadioInput bind:chosen={limit} options={[{ value: "Time period" }, { value: "Times opened" }]} />
 
-  <div class="save">
-    <a href="/">
-      <Button isFullWidth="true">save</Button>
-    </a>
-  </div>
-</div>
+<TimeInput columns={[hours, minutes]} />
 
-<div class="back">
-  <a href="/goal/edit/1">
-    <BackButton />
-  </a>
-</div>
+<Heading tag={5} className="text-center !font-normal">Limit Type</Heading>
+<RadioInput bind:chosen={type} options={[{ value: "Soft" }, { value: "Hard" }]} />
 
-<style lang="postcss">
-  /* F I X E D   I T E M S */
-  .back {
-    @apply absolute
-        top-5 left-3
-        min-h-fit min-w-fit;
-  }
-  .title {
-    @apply absolute
-            top-7;
-  }
-  .save {
-    @apply fixed
-        bottom-10
-        w-11/12;
-  }
-
-  /* C O N T E N T   E L E M E N T S */
-
-  .content {
-    @apply flex flex-col 
-        items-center place-content-center
-        select-none
-        mb-32 mt-28
-        gap-8
-        z-10;
-    pointer-events: all;
-    scrollbar-width: none;
-  }
-  .content .days {
-    @apply grid
-        grid-cols-1
-        items-center
-        place-items-center
-        select-none
-        w-11/12
-        gap-3;
-  }
-  .content .limit {
-    @apply grid
-        grid-cols-1
-        items-center
-        place-items-center
-        select-none
-        w-11/12
-        gap-5;
-  }
-  .content .type {
-    @apply grid
-        grid-cols-1
-        items-center
-        place-items-center
-        select-none
-        w-11/12
-        gap-3;
-  }
-</style>
+<a href="/goal/edit/2" class="fixed bottom-10 w-11/12"><Button isFullWidth="true">save</Button> </a>
+<a href="/goal" class="absolute top-5 left-3"><BackButton /></a>
