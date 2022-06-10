d16fac6407c16120d7bb0fc1f64a4cdd6ff64a78
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2022-06-10 20:46:22 +0200

    test(android): :white_check_mark: backend-fronted first succesful communication

---
+++ platforms/web/.routify/routes.default.js
@@ -0,0 +1,129 @@
+
+
+export default {
+  "meta": {},
+  "id": "_default",
+  "file": {
+    "path": "src/routes",
+    "dir": "src",
+    "base": "routes",
+    "ext": "",
+    "name": "routes"
+  },
+  "rootName": "default",
+  "routifyDir": import.meta.url,
+  "children": [
+    {
+      "meta": {},
+      "id": "_default_focus_svelte",
+      "name": "focus",
+      "module": () => import('../src/routes/focus.svelte'),
+      "file": {
+        "path": "src/routes/focus.svelte",
+        "dir": "src/routes",
+        "base": "focus.svelte",
+        "ext": ".svelte",
+        "name": "focus"
+      },
+      "children": []
+    },
+    {
+      "meta": {},
+      "id": "_default_goal_svelte",
+      "name": "goal",
+      "module": () => import('../src/routes/goal.svelte'),
+      "file": {
+        "path": "src/routes/goal.svelte",
+        "dir": "src/routes",
+        "base": "goal.svelte",
+        "ext": ".svelte",
+        "name": "goal"
+      },
+      "children": []
+    },
+    {
+      "meta": {},
+      "id": "_default_goaledit_svelte",
+      "name": "goaledit",
+      "module": () => import('../src/routes/goaledit.svelte'),
+      "file": {
+        "path": "src/routes/goaledit.svelte",
+        "dir": "src/routes",
+        "base": "goaledit.svelte",
+        "ext": ".svelte",
+        "name": "goaledit"
+      },
+      "children": []
+    },
+    {
+      "meta": {},
+      "id": "_default_goaledit2_svelte",
+      "name": "goaledit2",
+      "module": () => import('../src/routes/goaledit2.svelte'),
+      "file": {
+        "path": "src/routes/goaledit2.svelte",
+        "dir": "src/routes",
+        "base": "goaledit2.svelte",
+        "ext": ".svelte",
+        "name": "goaledit2"
+      },
+      "children": []
+    },
+    {
+      "meta": {},
+      "id": "_default_index_svelte",
+      "name": "index",
+      "module": () => import('../src/routes/index.svelte'),
+      "file": {
+        "path": "src/routes/index.svelte",
+        "dir": "src/routes",
+        "base": "index.svelte",
+        "ext": ".svelte",
+        "name": "index"
+      },
+      "children": []
+    },
+    {
+      "meta": {},
+      "id": "_default_presetEdit_svelte",
+      "name": "presetEdit",
+      "module": () => import('../src/routes/presetEdit.svelte'),
+      "file": {
+        "path": "src/routes/presetEdit.svelte",
+        "dir": "src/routes",
+        "base": "presetEdit.svelte",
+        "ext": ".svelte",
+        "name": "presetEdit"
+      },
+      "children": []
+    },
+    {
+      "meta": {},
+      "id": "_default_session_svelte",
+      "name": "session",
+      "module": () => import('../src/routes/session.svelte'),
+      "file": {
+        "path": "src/routes/session.svelte",
+        "dir": "src/routes",
+        "base": "session.svelte",
+        "ext": ".svelte",
+        "name": "session"
+      },
+      "children": []
+    },
+    {
+      "meta": {},
+      "id": "_default_summary_svelte",
+      "name": "summary",
+      "module": () => import('../src/routes/summary.svelte'),
+      "file": {
+        "path": "src/routes/summary.svelte",
+        "dir": "src/routes",
+        "base": "summary.svelte",
+        "ext": ".svelte",
+        "name": "summary"
+      },
+      "children": []
+    }
+  ]
+}
