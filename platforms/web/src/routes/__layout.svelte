d3bbdb005af4a5c083305aff27a5d17e501aeff1
matb85 <mateuszbis85@gmail.com>
2022-06-15 14:33:36 +0200

    feat(web): :zap: add route transitions

--- platforms/web/src/routes/__layout.svelte
+++ platforms/web/src/routes/__layout.svelte
@@ -7,6 +7,31 @@
 <script lang="ts">
   import "../app.css";
   import GlobalGradient from "@redinnlabs/system/utils/GlobalGradient.svelte";
+  import { beforeNavigate, goto } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  let body;
+  let tempTo: string;
+  onMount(() => {
+    body = document.getElementById("svelte");
+  });
+  beforeNavigate(({ from, cancel, to }) => {
+    if (from) {
+      new Promise<void>(resolve => {
+        body.classList.add("during-transition");
+        if (tempTo != to.pathname) {
+          cancel();
+          setTimeout(() => {
+            console.log(to.pathname);
+            goto(to.pathname);
+            body.classList.remove("during-transition");
+            resolve();
+          }, 300);
+        }
+        tempTo = to.pathname;
+      });
+    }
+  });
 </script>
 
 <slot />
@@ -15,4 +40,16 @@
 
 <style global>
   @import "@redinnlabs/system/utils/base.css";
+
+  :global(body) {
+    transition-duration: 300ms;
+    transition-property: transform, opacity;
+    transform: translateY(0rem);
+    opacity: 1;
+  }
+
+  :global(body.during-transition) {
+    transform: translateY(-5rem);
+    opacity: 0;
+  }
 </style>
