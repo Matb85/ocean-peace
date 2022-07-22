47597524c001f7117389f18f4b4fa3d70da9667d
matb85 <mateuszbis85@gmail.com>
2022-07-22 14:54:51 +0200

    feat(web): :sparkles: create sessionStorage manager

---
+++ platforms/web/src/lib/sessionManager/index.ts
@@ -0,0 +1,32 @@
+import goal from "./goal";
+export default {
+  goal,
+  selectors: {
+    set apps(value: string) {
+      sessionStorage.setItem("selectors_apps", value);
+    },
+    get apps() {
+      return sessionStorage.getItem("selectors_apps");
+    },
+    set backUrl(value: string) {
+      sessionStorage.setItem("selectors_backUrl", value);
+    },
+    get backUrl() {
+      return sessionStorage.getItem("selectors_backUrl");
+    },
+  },
+  action: {
+    set type(value: string) {
+      sessionStorage.setItem("action_type", value);
+    },
+    get type() {
+      return sessionStorage.getItem("action_type");
+    },
+    set backUrl(value: string) {
+      sessionStorage.setItem("action_back", value);
+    },
+    get backUrl() {
+      return sessionStorage.getItem("action_back");
+    },
+  },
+};
