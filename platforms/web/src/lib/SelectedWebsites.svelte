fc2f99df37a8862581cf1743181e097d60fa68e2
matb85 <mateuszbis85@gmail.com>
2022-08-12 14:39:57 +0200

    feat(web): :sparkles: add website selector

---
+++ platforms/web/src/lib/SelectedWebsites.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { CheckBoxStatic } from "@redinnlabs/system/Elements";
+  import { WebsiteTypes } from "$lib/utils/index";
+  import { Schedule } from "@redinnlabs/system/Units";
+
+  export let websites = [];
+</script>
+
+<div class="card-flex-col">
+  {#each websites as web}
+    <Schedule src={web.favicon} title={web.label} info={WebsiteTypes[web.type]} alt="">
+      <CheckBoxStatic state={true} />
+    </Schedule>
+  {/each}
+  {#if websites.length <= 0}
+    <p>0 selected</p>
+  {/if}
+</div>
