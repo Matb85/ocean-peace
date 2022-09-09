e370dda9367b870e31537b62252aa3632a1bb96e
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2022-09-10 00:01:43 +0200

    feat(mobile): :sparkles: JSONArrayGetIndexOf

--- platforms/mobile/android/app/src/main/java/com/oceanpeace/redinn/FunctionBase.java
+++ platforms/mobile/android/app/src/main/java/com/oceanpeace/redinn/FunctionBase.java
@@ -1,7 +1,13 @@
 package com.oceanpeace.redinn;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Calendar;
 import java.util.Objects;
@@ -11,71 +17,198 @@ public class FunctionBase {
     //region JSONArrayHasElement
 
     /**
-     * Checking if <i>JSONArray</i>  contains certain <b>int</b>
+     * Checking if {@link JSONArray#JSONArray()  JSONArray} contains certain <b>int</b>
      *
-     * @param array     array which you want to be searched through
-     * @param element   element you're looking for
-     * @return true if @array has element
-     * @throws JSONException
+     * @param array     {@link JSONArray#JSONArray()  JSONArray} which you want to be searched through
+     * @param element   <b>int</b> you're looking for
+     * @return returns {@link Integer#Integer(int) Integer} if array has element else <b>null</b>
+     * @throws JSONException throws {@link JSONException JSONException}
+     *
+     * @author Raccoon
+     * @since 09.09.2022
      */
-    public static boolean JSONArrayHasElement(JSONArray array, int element) throws JSONException {
+    @Nullable
+    public static Integer JSONArrayOptElement(@NonNull JSONArray array, int element) throws JSONException {
         for (int i=0; i < array.length(); i++) {
             if (array.getInt(i) == element)
-                return true;
+                return array.getInt(i);
         }
-        return false;
+        return null;
     }
 
     /**
-     * Checking if <i>JSONArray</i>  contains certain <b>String</b>
+     * Checking if {@link JSONArray#JSONArray()  JSONArray} contains certain {@link String#String() String}
      *
-     * @param array     array which you want to be searched through
-     * @param element   element you're looking for
-     * @return true if @array has element
-     * @throws JSONException
+     * @param array     {@link JSONArray#JSONArray()  JSONArray} which you want to be searched through
+     * @param element   {@link String#String() String} you're looking for
+     * @return returns {@link String#String() String} if array has element else <b>null</b>
+     * @throws JSONException throws {@link JSONException JSONException}
+     *
+     * @author Raccoon
+     * @since 09.09.2022
      */
-    public static boolean JSONArrayHasElement(JSONArray array, String element) throws JSONException {
+    @Nullable
+    public static String JSONArrayOptElement(@NonNull JSONArray array, @NonNull String element) throws JSONException {
         for (int i=0; i < array.length(); i++) {
             if (Objects.equals(array.getString(i), element))
-                return true;
+                return array.getString(i);
         }
-        return false;
+        return null;
     }
 
     /**
-     * Checking if <i>JSONArray</i> contains certain <b>long</b>
+     * Checking if {@link JSONArray#JSONArray()  JSONArray} contains certain <b>long</b>
      *
-     * @param array     array which you want to be searched through
-     * @param element   element you're looking for
-     * @return true if @array has element
-     * @throws JSONException
-     */
-    public static boolean JSONArrayHasElement(JSONArray array, long element) throws JSONException {
+     * @param array     {@link JSONArray#JSONArray()  JSONArray} which you want to be searched through
+     * @param element   <b>long</b> you're looking for
+     * @return return {@link Long#Long(long) Long} if <i>array</i> has element else <b>null</b>
+     * @throws JSONException throws {@link JSONException JSONException}
+     *
+     * @author Raccoon
+     * @since 09.09.2022
+     *      */
+    @Nullable
+    public static Long JSONArrayOptElement(@NonNull JSONArray array, long element) throws JSONException {
         for (int i=0; i < array.length(); i++) {
             if (array.getLong(i) == element)
-                return true;
+                return array.getLong(i);
         }
-        return false;
+        return null;
     }
 
     /**
      *
-     * Checks if the <i>array</i> contains JSONObject with a <b>String</b> in the specified <i>field</i>
+     * Checks if the <i>array</i> contains JSONObject with a {@link String#String() String} in the specified <i>field</i>
      *
-     * @param array     array which you want to be searched
-     * @param fieldName Name of the field of JSONObject that you want to search in
-     * @param element   <b>String</b> you looking for
-     * @return  <i>true</i> if @array has JSONObject containing element in specified field
-     * @throws JSONException
+     * @param array     {@link JSONArray#JSONArray()  JSONArray} which you want to be searched through
+     * @param fieldName Name of the field of {@link JSONObject#JSONObject() JSONObject} that you want to search in
+     * @param element   {@link String#String() String} you looking for
+     * @return returns {@link JSONObject#JSONObject() JSONObject} if array contains element in specified field else returns <i>null</i>
+     * @throws JSONException throws {@link JSONException JSONException}
+     *
+     * @author Raccoon
+     * @since 09.09.2022
      */
-    public static boolean JSONArrayHasElement(JSONArray array, String fieldName, String element) throws JSONException {
+    @Nullable
+    public static JSONObject JSONArrayOptElement(@NonNull JSONArray array, @NonNull String fieldName, @NonNull String element) throws JSONException {
         for (int i=0; i < array.length(); i++) {
             if (array.getJSONObject(i).getString(fieldName).equals(element))
-                return true;
+                return array.getJSONObject(i);
         }
-        return false;
+        return null;
     }
     //endregion
+
+
+
+    // region JSONArrayGetIndex
+
+
+    /**
+     * Get the index of the <b>element</b> int the <b>array</b>
+     *
+     * @param array     the searched {@link JSONArray#JSONArray() JSONArray}
+     * @param element   wanted {@link String#String() String}
+     * @return <i>int</i> with index of the element
+     *
+     * <br/>
+     * <b>NOTE: &emsp</b> if the function returned <b>-1</b>, it means that the array doesn't contain specified element
+     *
+     * @author Raccon
+     * @since 09.09.2022
+     */
+    public static int JSONArrayGetIndexOf(@NonNull JSONArray array, @NonNull String element) {
+        try {
+            for (int i=0; i<array.length(); i++) {
+                if (array.getString(i).equals(element))
+                    return i;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Get the index of the <b>element</b> int the <b>array</b>
+     *
+     * @param array     the searched {@link JSONArray#JSONArray() JSONArray}
+     * @param element   wanted <i>int</i>
+     * @return <i>int</i> with index of the element
+     *
+     * <br/>
+     * <b>NOTE: &emsp</b> if the function returned <b>-1</b>, it means that the array doesn't contain specified element
+     *
+     * @author Raccon
+     * @since 09.09.2022
+     */
+    public static int JSONArrayGetIndexOf(@NonNull JSONArray array, int element) {
+        try {
+            for (int i=0; i<array.length(); i++) {
+                if (array.getInt(i) == element)
+                    return i;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Get the index of the <b>element</b> int the <b>array</b>
+     *
+     * @param array     the searched {@link JSONArray#JSONArray() JSONArray}
+     * @param element   wanted <i>long</i>
+     * @return <i>int</i> with index of the element
+     *
+     * <br/>
+     * <b>NOTE: &emsp</b> if the function returned <b>-1</b>, it means that the array doesn't contain specified element
+     *
+     * @author Raccon
+     * @since 09.09.2022
+     */
+    public static int JSONArrayGetIndexOf(@NonNull JSONArray array, long element) {
+        try {
+            for (int i=0; i<array.length(); i++) {
+                if (array.getLong(i) == element)
+                    return i;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Get the index of the {@link JSONObject#JSONObject() JSONObject} with the <b>element</b> at the specified <b>fieldName</b>
+     *
+     * @param array     the searched {@link JSONArray#JSONArray() JSONArray}
+     * @param fieldName {@link String#String() String} containing name of the field which may contain <b>element</b>
+     * @param element   wanted {@link String#String() String}
+     * @return  <i>int</i> with index of the element
+     *
+     * <br/>
+     * <b>NOTE: &emsp</b> if the function returned <b>-1</b>, it means that the array doesn't contain specified element
+     *
+     * @author Raccon
+     * @since 09.09.2022
+     */
+    public static int JSONArrayGetIndexOf(@NonNull JSONArray array, @NonNull String fieldName, @NonNull String element) {
+        try {
+            for (int i=0; i<array.length(); i++) {
+                if (array.getJSONObject(i).getString(fieldName).equals(element))
+                    return i;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+
+    //endregion
+
+
 
     //region DayOfTheWeek
 
@@ -83,7 +216,10 @@ public class FunctionBase {
      *
      * Returns shorter version of day of the week <i>("Mon", etc.)</i>
      *
-     * @return <b>String</b> with day of the week
+     * @return {@link String#String() String} with day of the week
+     *
+     * @author Raccoon
+     * @since 09.09.2022
      */
     public static String getDayOfWeekStringShort() {
             int dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
@@ -116,4 +252,22 @@ public class FunctionBase {
 
     //endregion
 
+
+
+    //region Files Directory
+
+    /**
+     * Function providing {@link String#String() String} containing absolute path to context's files folder
+     *
+     * @param context application {@link Context#Context() Context}
+     * @return returns context's files directory
+     *
+     * @author Raccoon
+     * @since 09.09.2022
+     */
+    @NonNull
+    public static String getFilesDir(@NonNull Context context) {
+        return context.getFilesDir().getAbsolutePath();
+    }
+    //endregion
 }
