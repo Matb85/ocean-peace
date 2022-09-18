dd29efe99266ef9eca855f1ce6bc7344f7c55ef6
matb85 <mateuszbis85@gmail.com>
2022-09-18 16:20:24 +0200

    chore(mobile): :beers: disable the splash screen plugin temporarily

--- platforms/mobile/capacitor.config.ts
+++ platforms/mobile/capacitor.config.ts
@@ -5,17 +5,17 @@ const config: CapacitorConfig = {
   appName: "ocean-peace-mobile",
   bundledWebRuntime: false,
   webDir: "../web/build",
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 10000,
-      launchAutoHide: true,
-      backgroundColor: "ffffffff",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: false,
-      splashFullScreen: true,
-      splashImmersive: true,
-    },
-  },
+  // plugins: {
+  //   SplashScreen: {
+  //     launchShowDuration: 10000,
+  //     launchAutoHide: true,
+  //     backgroundColor: "ffffffff",
+  //     androidScaleType: "CENTER_CROP",
+  //     showSpinner: false,
+  //     splashFullScreen: true,
+  //     splashImmersive: true,
+  //   },
+  // },
   // comment out this line for production
   // adjust for development whenever necessary
   //server: { url: "http://192.168.1.6:3000", cleartext: true },
