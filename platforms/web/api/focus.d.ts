6bc691ffa5b37819dce072cbdf0d98eb147b66d4
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2022-12-15 23:22:37 +0100

    feat(web): :boom: create web api for focus

--- platforms/web/api/focus.d.ts
+++ platforms/web/api/focus.d.ts
@@ -3,5 +3,8 @@ interface FocusStartedI {
 }
 
 export interface FocusMethods {
-  
+  startStopwatch(packages: string): Promise<void>;
+  startContinuous(packages: string, duration: number): Promise<void>;
+  startPomodoro(packages: string, workDuration: number, breakDuration: number, cyclesNumber: number): Promise <void>;
+  stop(): Promise<void>;
 }
