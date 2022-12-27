4720f4d4dad441cd62abbed60e3404f41e4b73e6
matb85 <mateuszbis85@gmail.com>
2022-12-27 12:38:42 +0100

    chore: :bulb: update comments

--- platforms/web/api/usage.d.ts
+++ platforms/web/api/usage.d.ts
@@ -26,6 +26,18 @@ export interface UsageMethods {
    * @returns an array with 3 most used a a 4th element that represents the rest
    */
   getAppsUsedToday(): Promise<SingleAppUsageI[]>;
+  /** used in insights.svelte
+   * @returns a 10-element array, each element contains:
+   * the hour - time of the day
+   * the x position on the chart (just so it fits)
+   * the y position (the number of phone unlocks during that hour)
+   */
   getHourlyUsageToday(): Promise<HourlyUsageI[]>;
+  /** used in insights.svelte
+   * @returns a 7-element array, each element contains:
+   * the name of the day
+   * the number of minutes the phone was used for on that day
+   * boolean whether that day was successful or not (the goal screen time was exceeded or not)
+   */
   getScreenTimeHistory(): Promise<GoalHistoryI[]>;
 }
