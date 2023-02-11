fbfa08685b549a1eae654c5159273e29c1776035
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2023-02-12 00:26:11 +0100

    feat(mobile): :card_file_box: create Goal DAO

---
+++ platforms/mobile/android/app/src/main/java/com/redinn/oceanpeace/database/goals/GoalDAO.java
@@ -0,0 +1,33 @@
+package com.redinn.oceanpeace.database.goals;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface GoalDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Goal... goals);
+
+    @Update
+    void update(Goal... goals);
+
+    @Delete
+    void delete(Goal goal);
+
+    @Query("SELECT * FROM goal")
+    List<Goal> getAllGoals();
+
+    @Query("SELECT name FROM goal")
+    List<String> getAllGoalsName();
+
+    @Query("SELECT * FROM goal WHERE id = :id")
+    List<Goal> getGoalByName(String id);
+
+
+}
